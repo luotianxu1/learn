db3dde1e40d5649c6c7e7b4a4605a7b2dcd528ad
罗天旭 <1265062404@qq.com>
2023-12-14 08:10:28 +0800

    ts

---
+++ react/react-systematic/demo/src/setupProxy.js
@@ -0,0 +1,14 @@
+const { createProxyMiddleware } = require('http-proxy-middleware')
+
+module.exports = function (app) {
+    // app.use(
+    //     createProxyMiddleware('jian', {
+    //         target: '',
+    //         changeOrigin: true,
+    //         ws: true,
+    //         pathRewrite: {
+    //             '^/jian': '',
+    //         },
+    //     })
+    // )
+}
