16a5973123a2915927a9092019cec0f6e4ea1061
罗天旭 <1265062404@qq.com>
2023-12-14 20:30:06 +0800

    react

--- react/react-systematic/demo/src/index.jsx
+++ react/react-systematic/demo/src/index.jsx
@@ -3,4 +3,10 @@ import ReactDOM from 'react-dom/client'
 import '@/index.less'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<div>123</div>)
+let text = '123456'
+root.render(
+    <>
+        {text}
+        <p>789</p>
+    </>
+)
