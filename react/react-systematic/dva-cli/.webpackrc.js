2012c760547fa00b900628f7de5004b8b11032c6
罗天旭 <1265062404@qq.com>
2024-01-08 21:40:02 +0800

    dva-cli

--- react/react-systematic/dva-cli/.webpackrc.js
+++ react/react-systematic/dva-cli/.webpackrc.js
@@ -1,6 +1,11 @@
 import px2rem from "postcss-pxtorem";
 
 export default {
+  // 基本配置
+  hash: true,
+  html: {
+    template: "./public/index.ejs",
+  },
   // 对css的处理
   disableCSSModules: true,
   disableCSSSourceMap: true,
@@ -22,6 +27,20 @@ export default {
       target: "",
       changeOrigin: true,
       pathRewrite: { "^/api": "" },
+    },
+  },
+  // 配置浏览器兼容列表【区分环境】
+  env: {
+    development: {
+      extraBabelPlugins: ["dva-hmr"],
+      browserslist: [
+        "last 1 chrome version",
+        "last 1 firefox version",
+        "last 1 safari version",
+      ],
+    },
+    production: {
+      browserslist: [">0.2%", "not dead", "not op_mini all"],
     },
   },
 };
--- react/react-systematic/dva-cli/src/index.js
+++ react/react-systematic/dva-cli/src/index.js
@@ -1,4 +1,5 @@
 import dva from "dva";
+import "@babel/polyfill";
 
 // 1. Initialize
 const app = dva();
